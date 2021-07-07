5eb318a9371afac3ab2435482bea72ca050bf514
Amanda Koryl <77666069+amandakor@users.noreply.github.com>
2021-07-07 14:13:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup
 
+setup(
+    name='phage_pipeline',
+    version='0.0.1',
+    description='Package to create database with phage genomes',
+    url='git@github.com:bioinf-mcb/proj_koryl/phage_pipeline.git',
+    author='Amanda Koryl',
+    author_email='amanda.koryl@student.uj.edu.pl',
+    license='unlicense',
+    packages=['phage_pipeline'],
+    zip_safe=False
