89ffb5bfcd2900edad87ebaeedebd2072392d77b
Amanda Koryl <77666069+amandakor@users.noreply.github.com>
2021-12-19 19:16:13 +0100

    Delete phage_pipeline directory

--- phage_pipeline/download_summary.py
+++
@@ -1,4 +0,0 @@
-import urllib.request
-def download_summary():
-  '''function to download assembly_summary_refseq from NCBI FTP server'''
-  urllib.request.urlretrieve("https://ftp.ncbi.nlm.nih.gov/genomes/refseq/assembly_summary_refseq.txt", "assembly_summary_refseq.txt")
